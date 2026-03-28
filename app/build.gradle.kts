6b79c6a0e69840730b8c8b866ba7f70258aa912c
Vinícius Rodrigues de Uzêda <vruzeda@gmail.com>
2026-03-28 18:35:08 +0100

    Bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "com.vuzeda.animewatchlist.tracker"
         minSdk = 26
         targetSdk = 36
-        versionCode = 21
-        versionName = "3.6"
+        versionCode = 22
+        versionName = "3.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
