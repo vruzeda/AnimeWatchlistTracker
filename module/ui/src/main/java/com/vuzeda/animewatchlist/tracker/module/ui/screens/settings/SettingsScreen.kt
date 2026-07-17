c09dd04b249911fde9eb3890626a3c502e170401
Vinícius Rodrigues de Uzêda <vruzeda@gmail.com>
2026-07-17 08:34:01 +0200

    Fix Romaji preference name

--- module/ui/src/main/java/com/vuzeda/animewatchlist/tracker/module/ui/screens/settings/SettingsScreen.kt
+++ module/ui/src/main/java/com/vuzeda/animewatchlist/tracker/module/ui/screens/settings/SettingsScreen.kt
@@ -60,7 +60,7 @@ private enum class TitleLanguageOption(
     DEFAULT(TitleLanguage.DEFAULT, R.string.settings_title_language_default),
     ENGLISH(TitleLanguage.ENGLISH, R.string.settings_title_language_english),
     JAPANESE(TitleLanguage.JAPANESE, R.string.settings_title_language_japanese),
-    ROMAJI(TitleLanguage.ROMAJI, R.string.settings_title_language_japanese)
+    ROMAJI(TitleLanguage.ROMAJI, R.string.settings_title_language_romaji)
 }
 
 private enum class HomeViewModeOption(
